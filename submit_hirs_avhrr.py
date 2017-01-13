df2fd2eb33e9a0cb904bab4561c882e169c20d56
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2017-01-13 22:42:35 +0000

    Added a cluster submission script for the HIRS_AVHRR package.

---
+++ submit_hirs_avhrr.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import time
+import re
+import string
+from datetime import datetime, timedelta
+from calendar import monthrange
+import logging
+import traceback
+import getpass
+
+from subprocess import CalledProcessError, call
+from subprocess import Popen, STDOUT, PIPE
+
+from flo.time import TimeInterval
+from flo.ui import safe_submit_order
+
+from flo.sw.hirs_avhrr import HIRS_AVHRR
+
+# every module should have a LOG object
+LOG = logging.getLogger(__file__)
+
+# Set up the logging
+console_logFormat = '%(asctime)s : (%(levelname)s):%(filename)s:%(funcName)s:%(lineno)d:  %(message)s'
+dateFormat = '%Y-%m-%d %H:%M:%S'
+levels = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
+logging.basicConfig(stream=sys.stdout, level=levels[3],
+                    format=console_logFormat,
+                    datefmt=dateFormat)
+
+# General information
+comp = HIRS_AVHRR()
+
+# Latest Computation versions.
+hirs_version = 'v20151014'
+collo_version = 'v20151014'
+
+platform_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
+                    'noaa-12', 'noaa-14', 'noaa-15', 'noaa-16', 'noaa-17', 'noaa-18',
+                    'noaa-19', 'metop-a', 'metop-b']
+
+platform = 'metop-b'
+
+# Specify the intervals
+wedge = timedelta(seconds=1.)
+intervals = [
+    TimeInterval(datetime(2016, 12, 1),datetime(2016, 12, 2) - wedge),
+    #TimeInterval(datetime(2016, 3, 18), datetime(2016, 3, 19) - wedge)
+]
+
+LOG.info("Submitting intervals...")
+for interval in intervals:
+    LOG.info("Submitting interval {} -> {}".format(interval.left, interval.right))
+    contexts = comp.find_contexts(platform, hirs_version, collo_version, interval)
+    LOG.info("\tThere are {} contexts in this interval".format(len(contexts)))
+    contexts.sort()
+    for context in contexts:
+        print context
+    LOG.info("\tFirst context: {}".format(contexts[0]))
+    LOG.info("\tLast context:  {}".format(contexts[-1]))
+    LOG.info("\t{}".format(safe_submit_order(comp, [comp.dataset('out')], contexts, download_onlies=[])))
+
