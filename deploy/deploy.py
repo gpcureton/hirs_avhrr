1e163f336fe9399fe42b574a0210b390795a858c
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2018-02-19 13:34:50 -0600

    Delta_Catalog init information is now passed through the submissions scripts, rather than being hardcoded in hirs/source/flo/delta.py. Changed __file__ to __name__ in the logging setup. Using updated SIPS utility modules.

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -5,7 +5,7 @@ from flo_deploy.packagelib import *
 
 # every module should have a LOG object
 import logging, traceback
-LOG = logging.getLogger(__file__)
+LOG = logging.getLogger(__name__)
 
 class HIRS_AVHRR_Package(Package):
 
--- source/flo/__init__.py
+++ source/flo/__init__.py
@@ -1,38 +1,96 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+
+Purpose: Run the hirs_avhrr package
+
+Copyright (c) 2015 University of Wisconsin Regents.
+Licensed under GNU GPLv3.
+"""
 
 import os
+from os.path import basename, dirname, curdir, abspath, isdir, isfile, exists, splitext, join as pjoin
+import sys
+import logging
+import traceback
+
 from flo.computation import Computation
-from flo.subprocess import check_call
+from flo.builder import WorkflowNotReady
+from timeutil import TimeInterval, datetime, timedelta
 from flo.util import augmented_env, symlink_inputs_to_working_dir
-from flo.sw.hirs import HIRS
-from flo.sw.hirs.delta import delta_catalog
+#from flo.subprocess import check_call
+from flo.product import StoredProductCatalog
+
+import sipsprod
+from glutil import (
+    check_call,
+    #dawg_catalog,
+    #delivered_software,
+    #support_software,
+    #runscript,
+    #prepare_env,
+    #nc_gen,
+    nc_compress,
+    reraise_as,
+    #set_official_product_metadata,
+    FileNotFound
+)
+import flo.sw.hirs as hirs
+from flo.sw.hirs.delta import DeltaCatalog
 
 # every module should have a LOG object
-import logging, traceback
-LOG = logging.getLogger(__file__)
+LOG = logging.getLogger(__name__)
 
+SPC = StoredProductCatalog()
+
+def set_input_sources(input_locations):
+    global delta_catalog
+    delta_catalog = DeltaCatalog(**input_locations)
 
 class HIRS_AVHRR(Computation):
 
     parameters = ['granule', 'sat', 'hirs_version', 'collo_version']
     outputs = ['out']
 
+    @reraise_as(WorkflowNotReady, FileNotFound, prefix='NSS.GHRR')
     def build_task(self, context, task):
+        '''
+        Build up a set of inputs for a single context
+        '''
+        global delta_catalog
+
+        sensor = 'avhrr'
+        sat =  context['sat']
+        file_type = 'PTMSX'
+        granule = context['granule']
 
         hirs_context = context.copy()
         hirs_context.pop('collo_version')
+        LOG.debug("hirs_context: {}".format(hirs_context))
 
-        task.input('HIR1B', HIRS().dataset('out').product(hirs_context))
-        task.input('PTMSX', delta_catalog.file('avhrr', context['sat'], 'PTMSX',
-                                               context['granule']))
+        # Initialize the hirs module with the data locations
+        hirs.delta_catalog = delta_catalog
+        # Instantiate the hirs computation
+        hirs_comp =  hirs.HIRS()
 
+        LOG.debug("hirs input: {}".format(SPC.exists(hirs_comp.dataset('out').product(hirs_context))))
+
+        LOG.debug('Getting HIR1B input...')
+        task.input('HIR1B', hirs_comp.dataset('out').product(hirs_context))
+        LOG.debug('Getting PTMSX input...')
+        task.input('PTMSX', delta_catalog.file(sensor, sat, file_type, granule))
+
+    @reraise_as(WorkflowNotReady, FileNotFound, prefix='NSS.GHRR')
     def run_task(self, inputs, context):
+
+        LOG.debug("Running run_task()...")
 
         inputs = symlink_inputs_to_working_dir(inputs)
         output = 'hirs.avhrr.{}.{}.collo'.format(context['sat'], inputs['HIR1B'][12:30])
 
-        lib_dir = os.path.join(self.package_root, context['hirs_version'], 'lib')
+        lib_dir = pjoin(self.package_root, context['hirs_version'], 'lib')
 
-        cmd = os.path.join(self.package_root, context['hirs_version'], 'c++/hirs_avhrr')
+        cmd = pjoin(self.package_root, context['hirs_version'], 'c++/hirs_avhrr')
         cmd += ' {} {} {}'.format(inputs['HIR1B'], inputs['PTMSX'], output)
 
         print cmd
@@ -40,11 +98,20 @@ class HIRS_AVHRR(Computation):
 
         return {'out': output}
 
-    def find_contexts(self, sat, hirs_version, collo_version, time_interval):
+    def find_contexts(self, time_interval, sat, hirs_version, collo_version):
 
+        global delta_catalog
+
+        LOG.debug('delta_catalog.collection = {}'.format(delta_catalog.collection))
+        LOG.debug('delta_catalog.input_data = {}'.format(delta_catalog.input_data))
+
+        # Using HIR1B file info as the baseline for the required contexts
         files = delta_catalog.files('hirs', sat, 'HIR1B', time_interval)
+        #files = delta_catalog.files('avhrr', sat, 'PTMSX', time_interval)
+
         return [{'granule': file.data_interval.left,
                  'sat': sat,
                  'hirs_version': hirs_version,
                  'collo_version': collo_version}
-                for file in files]
+                for file in files
+                if file.data_interval.left >= time_interval.left]
