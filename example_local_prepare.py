fc2a3667f413388c582a080f74a655c975211b7a
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2018-05-05 04:50:45 +0000

    Completed local submission script and the main glue code.

--- example_local_prepare.py
+++ example_local_prepare.py
@@ -15,15 +15,19 @@ import logging
 from timeutil import TimeInterval, datetime, timedelta
 from flo.ui import local_prepare, local_execute
 
+import flo.sw.hirs2nc as hirs2nc
 import flo.sw.hirs_avhrr as hirs_avhrr
-from flo.sw.hirs.utils import setup_logging
+from flo.sw.hirs2nc.utils import setup_logging
 
 # every module should have a LOG object
 LOG = logging.getLogger(__name__)
 
+#
+# Local execution
+#
+
 # General information
-hirs_version  = 'v20151014'
-collo_version = 'v20151014'
+#collo_version = '0.1.65'
 wedge = timedelta(seconds=1.)
 
 # Satellite specific information
@@ -31,41 +35,43 @@ wedge = timedelta(seconds=1.)
 #granule = datetime(2017, 1, 1, 0, 32)
 #interval = TimeInterval(granule, granule+timedelta(seconds=0))
 
-# Data locations
-collection = {'HIR1B': 'ILIAD',
-              'CFSR': 'DELTA',
-              'PTMSX': 'ILIAD'}
-# NOAA-19
-#satellite = 'noaa-19'
-#input_data = {'HIR1B': '/mnt/sdata/geoffc/HIRS_processing/data_lists/NOAA-19/HIR1B_noaa-19_latest',
-              #'CFSR':  '/mnt/sdata/geoffc/HIRS_processing/data_lists/CFSR.out',
-              #'PTMSX': '/mnt/sdata/geoffc/HIRS_processing/data_lists/NOAA-19/PTMSX_noaa-19_latest'}
+def setup_computation(satellite):
 
-# Metop-B
-#satellite = 'metop-b'
-input_data = {'HIR1B': '/mnt/cephfs_data/geoffc/hirs_data_lists/Metop-B/HIR1B_metop-b_latest',
-              'CFSR':  '/mnt/cephfs_data/geoffc/hirs_data_lists/CFSR.out',
-              'PTMSX': '/mnt/cephfs_data/geoffc/hirs_data_lists/Metop-B/PTMSX_metop-b_latest'}
+    #satellite = 'metop-b'
+    input_data = {'HIR1B': '/mnt/software/flo/hirs_l1b_datalists/{0:}/HIR1B_{0:}_latest'.format(satellite),
+                  'CFSR':  '/mnt/cephfs_data/geoffc/hirs_data_lists/CFSR.out',
+                  'PTMSX': '/mnt/software/flo/hirs_l1b_datalists/{0:}/PTMSX_{0:}_latest'.format(satellite)}
 
-input_sources = {'collection':collection, 'input_data':input_data}
+    # Data locations
+    collection = {'HIR1B': 'ILIAD',
+                  'CFSR': 'DELTA',
+                  'PTMSX': 'FJORD'}
 
-# Initialize the hirs_avhrr module with the data locations
-hirs_avhrr.set_input_sources(input_sources)
+    input_sources = {'collection':collection, 'input_data':input_data}
 
-# Instantiate the computation
-comp = hirs_avhrr.HIRS_AVHRR()
+    # Initialize the hirs2nc module with the data locations
+    hirs_avhrr.set_input_sources(input_sources)
+
+    # Instantiate the computation
+    comp = hirs_avhrr.HIRS_AVHRR()
+
+    return comp
+
 
 #
 # Local execution
 #
 
-def local_execute_example(interval, satellite, hirs_version, collo_version,
+def local_execute_example(interval, satellite, hirs2nc_delivery_id, collo_version,
                           skip_prepare=False, skip_execute=False, verbosity=2):
 
     setup_logging(verbosity)
 
+    comp = setup_computation(satellite)
+    hirs2nc_comp = hirs2nc.HIRS2NC()
+
     # Get the required context...
-    contexts =  comp.find_contexts(interval, satellite, hirs_version, collo_version)
+    contexts =  comp.find_contexts(interval, satellite, hirs2nc_delivery_id, collo_version)
 
     if len(contexts) != 0:
         LOG.info("Candidate contexts in interval...")
@@ -76,7 +82,7 @@ def local_execute_example(interval, satellite, hirs_version, collo_version,
             if not skip_prepare:
                 LOG.info("Running hirs_avhrr local_prepare()...")
                 LOG.info("Preparing context... {}".format(contexts[0]))
-                local_prepare(comp, contexts[0])
+                local_prepare(comp, contexts[0], download_only=[hirs2nc_comp])
             if not skip_execute:
                 LOG.info("Running hirs_avhrr local_execute()...")
                 LOG.info("Running context... {}".format(contexts[0]))
@@ -87,14 +93,16 @@ def local_execute_example(interval, satellite, hirs_version, collo_version,
     else:
         LOG.error("There are no valid {} contexts for the interval {}.".format(satellite, interval))
 
-def print_contexts(interval, satellite, hirs_version, collo_version, verbosity=2):
+def print_contexts(interval, satellite, hirs2nc_delivery_id, collo_version, verbosity=2):
+
     setup_logging(verbosity)
-    contexts = comp.find_contexts(interval, satellite, hirs_version, collo_version)
+
+    comp = setup_computation(satellite)
+
+    contexts = comp.find_contexts(interval, satellite, hirs2nc_delivery_id, collo_version)
     for context in contexts:
         LOG.info(context)
 
 #platform_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
                     #'noaa-12', 'noaa-14', 'noaa-15', 'noaa-16', 'noaa-17', 'noaa-18',
                     #'noaa-19', 'metop-a', 'metop-b']
-
-# local_execute_example(granule, platform, hirs_version, collo_version)
--- source/flo/__init__.py
+++ source/flo/__init__.py
@@ -12,13 +12,15 @@ import os
 from os.path import basename, dirname, curdir, abspath, isdir, isfile, exists, splitext, join as pjoin
 import sys
 import logging
+import shutil
 import traceback
+from glob import glob
+from subprocess import CalledProcessError
 
 from flo.computation import Computation
 from flo.builder import WorkflowNotReady
 from timeutil import TimeInterval, datetime, timedelta
-from flo.util import augmented_env, symlink_inputs_to_working_dir
-#from flo.subprocess import check_call
+from flo.util import augmented_env
 from flo.product import StoredProductCatalog
 
 import sipsprod
@@ -26,22 +28,40 @@ from glutil import (
     check_call,
     #dawg_catalog,
     #delivered_software,
-    #support_software,
-    #runscript,
-    #prepare_env,
+    support_software,
+    runscript,
+    prepare_env,
     #nc_gen,
     nc_compress,
     reraise_as,
     #set_official_product_metadata,
     FileNotFound
 )
-import flo.sw.hirs as hirs
-from flo.sw.hirs.delta import DeltaCatalog
+import flo.sw.hirs2nc as hirs2nc
+from flo.sw.hirs2nc.delta import DeltaCatalog
 
 # every module should have a LOG object
 LOG = logging.getLogger(__name__)
 
 SPC = StoredProductCatalog()
+
+sat_code_to_satellite = {
+    'NA' : 'noaa-06',
+    'NC' : 'noaa-07',
+    'NE' : 'noaa-08',
+    'NF' : 'noaa-09',
+    'NG' : 'noaa-10',
+    'NH' : 'noaa-11',
+    'ND' : 'noaa-12',
+    'NJ' : 'noaa-14',
+    'NK' : 'noaa-15',
+    'NL' : 'noaa-16',
+    'NM' : 'noaa-17',
+    'NN' : 'noaa-18',
+    'NP' : 'noaa-19',
+    'M2' : 'metop-a',
+    'M1' : 'metop-b' 
+    }
 
 def set_input_sources(input_locations):
     global delta_catalog
@@ -49,7 +69,7 @@ def set_input_sources(input_locations):
 
 class HIRS_AVHRR(Computation):
 
-    parameters = ['granule', 'sat', 'hirs_version', 'collo_version']
+    parameters = ['granule', 'satellite', 'hirs2nc_delivery_id', 'collo_version']
     outputs = ['out']
 
     @reraise_as(WorkflowNotReady, FileNotFound, prefix='NSS.GHRR')
@@ -60,7 +80,7 @@ class HIRS_AVHRR(Computation):
         global delta_catalog
 
         sensor = 'avhrr'
-        sat =  context['sat']
+        satellite =  context['satellite']
         file_type = 'PTMSX'
         granule = context['granule']
 
@@ -68,37 +88,103 @@ class HIRS_AVHRR(Computation):
         hirs_context.pop('collo_version')
         LOG.debug("hirs_context: {}".format(hirs_context))
 
-        # Initialize the hirs module with the data locations
-        hirs.delta_catalog = delta_catalog
-        # Instantiate the hirs computation
-        hirs_comp =  hirs.HIRS()
+        # Initialize the hirs2nc module with the data locations
+        hirs2nc.delta_catalog = delta_catalog
+        # Instantiate the hirs2nc computation
+        hirs2nc_comp = hirs2nc.HIRS2NC()
 
-        LOG.debug("hirs input: {}".format(SPC.exists(hirs_comp.dataset('out').product(hirs_context))))
+        LOG.debug("hirs input: {}".format(SPC.exists(hirs2nc_comp.dataset('out').product(hirs_context))))
 
         LOG.debug('Getting HIR1B input...')
-        task.input('HIR1B', hirs_comp.dataset('out').product(hirs_context))
+        task.input('HIR1B', hirs2nc_comp.dataset('out').product(hirs_context))
         LOG.debug('Getting PTMSX input...')
-        task.input('PTMSX', delta_catalog.file(sensor, sat, file_type, granule))
+        task.input('PTMSX', delta_catalog.file(sensor, satellite, file_type, granule))
+
+    def hirs_avhrr_collocation(self, inputs, context):
+        '''
+        Run the collocation executable.
+        '''
+
+        rc = 0
+        
+        LOG.info('inputs = {}'.format(inputs))
+
+        satellite = context['satellite']
+        collo_version = context['collo_version']
+
+        hirs_file = inputs['HIR1B']
+        patmosx_file = inputs['PTMSX']
+
+        # Where are we running the package
+        work_dir = abspath(curdir)
+        LOG.debug("working dir = {}".format(work_dir))
+
+        # Removing the output file if it exists
+        granule_datestamp = '.'.join(basename(hirs_file).split('.')[3:8])
+        output_file = 'colloc.hirs.avhrr.{}.{}.v{}.hdf'.format(satellite, granule_datestamp, collo_version)
+        if os.path.exists(output_file):
+            LOG.info('{} exists, removing...'.format(output_file))
+            os.remove(output_file)
+
+        # Get the required collocation exe
+        hirs_avhrr = support_software.lookup('collopak', version=collo_version)
+        hirs_avhrr_bin = pjoin(hirs_avhrr.path,'bin/hirs_avhrr')
+
+        # Get the required  environment variables
+        env = prepare_env([hirs_avhrr])
+        LOG.debug(env)
+
+        cmd = '{} {} {}'.format(hirs_avhrr_bin, hirs_file, patmosx_file)
+        #cmd = 'sleep 1; touch colloc.avhrr_metopb.avhrr_metopb.20140101T000900_000900.nc'
+
+        try:
+            LOG.debug("cmd = \\\n\t{}".format(cmd.replace(' ',' \\\n\t')))
+            rc_hirs_avhrr = 0
+            runscript(cmd, requirements=[], env=env)
+        except CalledProcessError as err:
+            rc_hirs_avhrr = err.returncode
+            LOG.error("hirs_avhrr binary {} returned a value of {}".format(hirs_avhrr_bin, rc_hirs_avhrr))
+            return rc_hirs_avhrr, []
+
+        # Move vnpaerdt file to the output directory
+        temp_output_file = pjoin(work_dir, 'colloc.*.nc')
+        temp_output_file = glob(temp_output_file)
+        if len(temp_output_file) != 0:
+            temp_output_file = temp_output_file[0]
+            LOG.info('Found collocation file "{}", moving to {}...'.format(temp_output_file, output_file))
+            shutil.move(temp_output_file, output_file)
+            output_file = glob(output_file)[0]
+        else:
+            LOG.error('There are no output collocation file "{}", aborting'.format(output_file))
+            rc = 1
+            return rc, []
+
+        LOG.info('Finished collocation routine')
+
+        return rc, output_file
 
     @reraise_as(WorkflowNotReady, FileNotFound, prefix='NSS.GHRR')
     def run_task(self, inputs, context):
 
         LOG.debug("Running run_task()...")
 
-        inputs = symlink_inputs_to_working_dir(inputs)
-        output = 'hirs.avhrr.{}.{}.collo'.format(context['sat'], inputs['HIR1B'][12:30])
+        for key in context.keys():
+            LOG.debug("run_task() context['{}'] = {}".format(key, context[key]))
 
-        lib_dir = pjoin(self.package_root, context['hirs_version'], 'lib')
+        granule = context['granule']
 
-        cmd = pjoin(self.package_root, context['hirs_version'], 'c++/hirs_avhrr')
-        cmd += ' {} {} {}'.format(inputs['HIR1B'], inputs['PTMSX'], output)
+        rc, colloc_file = self.hirs_avhrr_collocation(inputs, context)
 
-        print cmd
-        check_call(cmd, shell=True, env=augmented_env({'LD_LIBRARY_PATH': lib_dir}))
+        LOG.debug('hirs_avhrr_collocation() return value: {}'.format(rc))
+        LOG.info('hirs_avhrr_collocation() generated {}'.format(colloc_file))
 
-        return {'out': output}
+        # The staging routine assumes that the output file is located in the work directory
+        # "tmp******", and that the output path is to be prepended, so return the basename.
+        output = basename(colloc_file)
 
-    def find_contexts(self, time_interval, sat, hirs_version, collo_version):
+        return {'out': nc_compress(output)}
+
+    def find_contexts(self, time_interval, satellite, hirs2nc_delivery_id, collo_version):
 
         global delta_catalog
 
@@ -106,12 +192,12 @@ class HIRS_AVHRR(Computation):
         LOG.debug('delta_catalog.input_data = {}'.format(delta_catalog.input_data))
 
         # Using HIR1B file info as the baseline for the required contexts
-        files = delta_catalog.files('hirs', sat, 'HIR1B', time_interval)
-        #files = delta_catalog.files('avhrr', sat, 'PTMSX', time_interval)
+        files = delta_catalog.files('hirs', satellite, 'HIR1B', time_interval)
+        #files = delta_catalog.files('avhrr', satellite, 'PTMSX', time_interval)
 
         return [{'granule': file.data_interval.left,
-                 'sat': sat,
-                 'hirs_version': hirs_version,
+                 'satellite': satellite,
+                 'hirs2nc_delivery_id': hirs2nc_delivery_id,
                  'collo_version': collo_version}
                 for file in files
                 if file.data_interval.left >= time_interval.left]
